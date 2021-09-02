492c7419032419f9aee4d49bf45a6f0050f3296c
alcho01 <alexbriancho@gmail.com>
2021-09-01 23:51:43 -0400

    Doing Part 2 - Instructions

---
+++ Experiments/Drawing-Experiment/js/script.js
@@ -0,0 +1,32 @@
+/**
+Title of Project
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+/**
+Description of setup
+*/
+function setup() {
+  background(127,222,10);
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
