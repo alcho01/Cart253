58f2a087bf37217a3aa55d436a5a55e06e4cce18
alcho01 <alexbriancho@gmail.com>
2021-09-09 15:21:18 -0400

    Finished Drawing An Alien

--- Activities/02-draw-an-alien/js/script.js
+++ Activities/02-draw-an-alien/js/script.js
@@ -1,27 +1,45 @@
 /**
-Title of Project
-Author Name
+Creating An Alien
+Alex Cho
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
 */
 
-"use strict";
+//Creating The Canvas
+  function setup() {
+  createCanvas(640, 480);
 
+//Set the background to
+  background(0 ,0 ,153);
+  noStroke();
 
-/**
-Description of preload
-*/
-function preload() {
+//Making The Body
+  fill(64);
+  ellipse(320, 480, 300, 200);
 
-}
+//Making The Head
+  fill(64);
+  ellipse(320, 240, 250, 400);
 
+//Making The Eyes
+  fill(0);
+  ellipse(250, 240, 80, 250);
+  ellipse(390, 240, 80, 250);
 
-/**
-Description of setup
-*/
-function setup() {
-  background(127,222,10);
+//Making White in Eyes
+  fill(255);
+  ellipse(230, 220, 30, 50);
+  ellipse(370, 220, 30, 50);
+
+//Making The Nose
+  fill(0);
+  ellipse(300, 350, 10, 10);
+  ellipse(340, 350, 10, 10);
+
+//Making The Mouth
+  stroke(200, 0, 0);
+  strokeWeight(10);
+  rectMode(CENTER);
+  rect(320, 390, 70, 20);
 }
 
 
