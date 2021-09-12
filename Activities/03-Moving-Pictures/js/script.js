d534e14f827187675306445b98f57896f1341633
alcho01 <alexbriancho@gmail.com>
2021-09-12 01:24:14 -0400

    A3: Started the moving pictures activity

---
+++ Activities/03-Moving-Pictures/js/script.js
@@ -0,0 +1,28 @@
+/**
+I Like To Move It
+Alex Cho
+
+*/
+
+//Shape Variables
+let bg = {
+
+};
+let circle1 = {
+
+};
+let circle2 = {
+
+};
+
+
+
+function setup() {
+  background(127,222,10);
+}
+
+
+
+function draw() {
+
+}
