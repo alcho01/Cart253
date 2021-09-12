39307d95465a60f2a0d9786d6918e1743d14746c
alcho01 <alexbriancho@gmail.com>
2021-09-12 02:11:31 -0400

    A3: Completed the activity

--- Activities/03-Moving-Pictures/js/script.js
+++ Activities/03-Moving-Pictures/js/script.js
@@ -1,28 +1,58 @@
 /**
-I Like To Move It
+Moving Pictures
 Alex Cho
 
 */
 
-//Shape Variables
+//Variables
 let bg = {
-
+  r: 0,
+  g: 0,
+  b: 0
 };
 let circle1 = {
-
+  x: 0,
+  y: 250,
+  size: 100,
+  fill: 255,
+  alpha: 200,
+  speed: 1,
+  growth: 0.25,
+  relativeSize: 0.9
 };
 let circle2 = {
-
+  x: 500,
+  y: 250,
+  size: 50,
+  fill: 255,
+  alpha: 200,
+  speed: -1
 };
 
-
-
+//Setup
 function setup() {
-  background(127,222,10);
+  createCanvas(500,500);
+  noStroke();
 }
 
-
-
+//Background
 function draw() {
+  background(bg.r,bg.g,bg.b);
+  bg.r = map(circle1.size,100,width,0,255);
+
+//Circle 1
+  circle1.x = circle1.x + circle1.speed;
+  circle1.x = constrain(circle1.x,0,width/2);
+  circle1.size = circle1.size + circle1.growth;
+  circle1.size = constrain(circle1.size,0,width);
+  fill(circle1.fill,circle1.alpha);
+  ellipse(circle1.x,circle1.y,circle1.size);
+
+//Circle 2
+  circle2.x = circle2.x + circle2.speed;
+  circle2.x = constrain(circle2.x,width/2,width);
+  circle2.size = circle1.size * circle1.relativeSize;
+  fill(circle2.fill,circle2.alpha);
+  ellipse(circle2.x,circle2.y,circle2.size);
 
 }
