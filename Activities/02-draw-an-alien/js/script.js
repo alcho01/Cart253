84ae3aff991a0da5cf6ab155e330fc4531f8864e
alcho01 <alexbriancho@gmail.com>
2021-09-14 23:48:16 -0400

    Finished Up The Alien

--- Activities/02-draw-an-alien/js/script.js
+++ Activities/02-draw-an-alien/js/script.js
@@ -9,37 +9,70 @@ Alex Cho
   createCanvas(640, 480);
 
 //Set the background to
-  background(0 ,0 ,153);
+  background(13, 116, 157);
   noStroke();
 
+//UFO Light
+    fill(249, 208, 65);
+    rect(495,100,70,430);
+
+//UFO
+  fill(217, 222, 221);
+  ellipse(530,100,250,40);
+  ellipse(530,90,160,40);
+
 //Making The Body
-  fill(64);
-  ellipse(320, 480, 300, 200);
+  fill(49, 185, 132);
+  ellipse(320, 500, 300, 200);
+
+//Making The Neck
+  fill(60, 185, 132);
+  rect(320,520,60,60);
 
 //Making The Head
-  fill(64);
-  ellipse(320, 240, 250, 400);
+  fill(49, 185, 132);
+  ellipse(320, 150, 200, 200);
+  ellipse(320,200,250,230);
+  ellipse(320,250,200,200);
+  ellipse(320,300,150,170);
+
 
 //Making The Eyes
   fill(0);
-  ellipse(250, 240, 80, 250);
-  ellipse(390, 240, 80, 250);
+  ellipse(250, 190, 100, 80);
+  ellipse(390, 190, 100, 80);
 
 //Making White in Eyes
   fill(255);
-  ellipse(230, 220, 30, 50);
-  ellipse(370, 220, 30, 50);
+  ellipse(210, 190, 10, 20);
+  ellipse(270, 190, 30, 40);
+  ellipse(370, 190, 30, 40);
+  ellipse(430, 190, 10, 20);
 
 //Making The Nose
   fill(0);
-  ellipse(300, 350, 10, 10);
-  ellipse(340, 350, 10, 10);
+  ellipse(310, 240, 10, 40);
+  ellipse(330, 240, 10, 40);
 
 //Making The Mouth
-  stroke(200, 0, 0);
-  strokeWeight(10);
   rectMode(CENTER);
-  rect(320, 390, 70, 20);
+  rect(320, 320, 90, 5);
+
+//Making The Neck
+  fill(49, 185, 132);
+  rect(320, 400, 20, 50);
+
+//Making The Hand
+  fill(49, 185, 132);
+  ellipse(150,480,80,40);
+  ellipse(100,480,50,340);
+  ellipse(100,320,50,30);
+  ellipse(78,305,10,40);
+  ellipse(90,305,10,50);
+  ellipse(102,305,10,50);
+  ellipse(114,305,10,50);
+  ellipse(124,315,10,20);
+
 }
 
 
