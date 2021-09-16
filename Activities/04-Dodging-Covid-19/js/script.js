961d5edfb1f7024899a81f86b746ce8d3a038baf
alcho01 <alexbriancho@gmail.com>
2021-09-16 16:19:58 -0400

    A4 Added the Canvas,Background,Movement and shape of covid19

--- Activities/04-Dodging-Covid-19/js/script.js
+++ Activities/04-Dodging-Covid-19/js/script.js
@@ -22,21 +22,32 @@ let covid19 = {
   }
 };
 
-/**
-Description of setup
-*/
+//Creating The Canvas
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+//Setting up the height and speed
   covid19.y = random(0,height);
   covid19.vx = covid19.speed;
-
 
 }
 
 
-/**
-Description of draw()
-*/
+//Background
 function draw() {
+  background(17,20,94);
+//Movement
+  covid19.x = covid19.x + covid19.vx;
+  covid19.y = covid19.y + covid19.vy;
+//Filling Color for Covid19
+  fill(covid19.fill.r,covid19.fill.g,covid19.fill.b);
+  noStroke();
+//Creating The Covid19 Shape
+  ellipse(covid19.x,covid19.y,covid19.size);
+
+
+
+
+
 
 }
