a0ac1bdf68e7045b6fc3dac765d37cad3373bdf6
alcho01 <alexbriancho@gmail.com>
2021-09-16 16:34:09 -0400

    A4 Created the User, Created "If" statement.

--- Activities/04-Dodging-Covid-19/js/script.js
+++ Activities/04-Dodging-Covid-19/js/script.js
@@ -21,6 +21,12 @@ let covid19 = {
     b: 0
   }
 };
+let user = {
+  x: 200,
+  y: 200,
+  size: 100,
+  fill: 255
+};
 
 //Creating The Canvas
 function setup() {
@@ -36,15 +42,30 @@ function setup() {
 //Background
 function draw() {
   background(17,20,94);
-//Movement
+
+//Covidd19 Movement
   covid19.x = covid19.x + covid19.vx;
   covid19.y = covid19.y + covid19.vy;
+
+  if (covid19.x > width) {
+    covid19.x = 0;
+    covid19.y = random(0,height);
+  }
+
+//User Movement
+  user.x = mouseX;
+  user.y = mouseY;
+
 //Filling Color for Covid19
   fill(covid19.fill.r,covid19.fill.g,covid19.fill.b);
   noStroke();
+
 //Creating The Covid19 Shape
   ellipse(covid19.x,covid19.y,covid19.size);
 
+//The user
+fill(user.fill);
+ellipse(user.x,user.y,user.size);
 
 
 
