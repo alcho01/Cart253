dcbe1c142b91badaf4121a002b21c46abf7495f8
alcho01 <alexbriancho@gmail.com>
2021-09-22 19:38:59 -0400

    E1: Started Exercise 1, Changed title and description.

---
+++ Exercises/I-Like-To-Move-It/js/script.js
@@ -0,0 +1,37 @@
+/**
+I Like To Move It
+Alex Cho
+
+Include three shapes
+Include movement
+Include size changes
+Include color changes
+Use map() and constrain()
+Respond to the mouse position using mouseX and mouseY
+***********************/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
