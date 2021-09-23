35d9bbeea2f34e84801c2094015014b30abb1bc4
alcho01 <alexbriancho@gmail.com>
2021-09-23 17:06:09 -0400

    P1 Testing some Variable to see if it will make the desired shape

--- Projects/Project 1/js/script.js
+++ Projects/Project 1/js/script.js
@@ -16,6 +16,25 @@ let bg = {
   r: 137,
   g: 178,
   b: 245
+};
+let santaup = {
+  x: 300,
+  y: 400,
+  size: 50,
+  growth:5,
+  fill:{
+    r:237,
+    g:205,
+    b:178
+  }
+};
+let santalow = {
+  x: 200,
+  y: 200,
+  w: 80,
+  h: 80,
+  start: 0,
+  stop: PI
 }
 
 /**
@@ -23,7 +42,7 @@ Description of setup
 */
 function setup() {
   createCanvas(600,800);
-
+  noStroke();
 }
 
 
@@ -31,6 +50,19 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  backgrd();
+
+
+function backgrd() {
   background(bg.r, bg.g, bg.b);
   bg.b = map(mouseY, 0, 600, 255, 230); //Lighter To Darker Blue
+
+}
+
+function santa() {
+
+
+}
+
+
 }
