ff0ffa3736ed8ea1415bf8746acbe4ef5589b069
alcho01 <alexbriancho@gmail.com>
2021-09-23 15:56:31 -0400

    P1: Started Project 1

---
+++ Projects/Project 1/js/script.js
@@ -0,0 +1,38 @@
+/**
+Title of Project
+Alex Cho
+
+At Least 2 Moving Elements
+Interactivity
+Aesthetic?
+TitleScreen
+EndScreen
+
+Idea:
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
