1c7413485910f12b2ab5338891be152b8511da47
alcho01 <alexbriancho@gmail.com>
2021-09-26 15:55:42 -0400

    E2: Defined Burger, yet to show on screen, set burger variable and settings.

--- Exercises/Dodge-Em/js/script.js
+++ Exercises/Dodge-Em/js/script.js
@@ -19,7 +19,7 @@ let bg = {
 //preloading my images
 function preload() {
   //man.image = loadImage('assets/images/man.png'); //Made in Illustrator
-  //hamburger.image = loadImage('assets/images/hamburger.png'); //Made in Illustrator
+  hamburger.image = loadImage('assets/images/hamburger.png'); //Made in Illustrator
 }
 
 function setup() {
@@ -39,4 +39,22 @@ backgrnd();
 //Defining Background
 function backgrnd() {
   background(bg.r,bg.g,bg.b);
+}
+
+//Hamburger Settings
+function hamburger(){
+  let burger = {
+    x: 0,
+    y: 250,
+    w: 100,
+    h: 100,
+    vx: 0,
+    vy: 0,
+    speed: 10
+  };
+
+//Defining The Hamburger
+  imageMode(CENTER);
+  image(hamburger.image,burger.x,burger.y,burger.w,burger.h);
+
 }
