2292833139440f885885f1533c4f1cc1b2cb9190
alcho01 <alexbriancho@gmail.com>
2021-09-26 03:47:26 -0400

    E2 Started Dodge-Em

---
+++ Exercises/Dodge-Em/js/script.js
@@ -0,0 +1,35 @@
+/**
+Dodge-Em
+Alex Cho
+
+Change the way the user controls their circle
+Add at least one new if-statement
+Change the way the simulation looks
+Use at least one image
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
