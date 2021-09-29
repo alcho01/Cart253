e23e25b74685b4a540c12ce7309824baaa8a90f1
alcho01 <alexbriancho@gmail.com>
2021-09-29 14:28:36 -0400

    E3 Added more image files created from Illustrator, displayed the usercar.

--- Exercises/love-actually/js/script.js
+++ Exercises/love-actually/js/script.js
@@ -19,27 +19,50 @@ let bg = {
   w: 800,
   h: 800,
 
-}
+};
+
+let user = {
+  x: undefined,
+  y: undefined,
+  w: 150,
+  h: 150,
+  vx: -5,
+  vx2: 5,
+  vy: -5,
+  vy2: 5,
+  image: undefined
+};
 
 
 function preload() {
   background.image = loadImage('assets/images/background.png');
+  user.image = loadImage('assets/images/usercar.png');
 }
-
 
 
 function setup() {
+//Setting Up the Canvas
   createCanvas(800,800);
 
+//User Car Spawn
+  user.x = width/2;
+  user.y = height/2;
+
 }
-
-
 
 function draw() {
   bgset();
+  display();
 }
 
+//DISPLAY BACKGROUND
 function bgset() {
   imageMode(CENTER);
   image(background.image,bg.x,bg.y,bg.w,bg.h);
+}
+
+//DISPLAY USER CAR
+function display() {
+  imageMode(CENTER);
+  image(user.image,user.x,user.y,user.w,user.h);
 }
