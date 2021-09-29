336569ba2312f2d7d0277bb0c9e8445fe7dbe3d0
alcho01 <alexbriancho@gmail.com>
2021-09-29 14:00:18 -0400

    E3 Added a background created from illustrator

--- Exercises/love-actually/js/script.js
+++ Exercises/love-actually/js/script.js
@@ -13,20 +13,33 @@ DUE OCTOBER 7th 2021
 
 "use strict";
 
+let bg = {
+  x: 400,
+  y: 400,
+  w: 800,
+  h: 800,
+
+}
 
 
 function preload() {
-
+  background.image = loadImage('assets/images/background.png');
 }
 
 
 
 function setup() {
+  createCanvas(800,800);
 
 }
 
 
 
 function draw() {
+  bgset();
+}
 
+function bgset() {
+  imageMode(CENTER);
+  image(background.image,bg.x,bg.y,bg.w,bg.h);
 }
