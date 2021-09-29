7e38963bc78d2fc7c1e83bbe53ec66e5df75df95
alcho01 <alexbriancho@gmail.com>
2021-09-29 13:12:11 -0400

    E3 Started love-actually

---
+++ Exercises/love-actually/js/script.js
@@ -0,0 +1,32 @@
+/**
+Love-Actually
+Alex Cho
+
+GUIDELINES
+Allow the user to control one of the circles
+Make the non-user circle move differently
+Add at least one extra function
+Add at least one extra ending
+
+DUE OCTOBER 7th 2021
+*/
+
+"use strict";
+
+
+
+function preload() {
+
+}
+
+
+
+function setup() {
+
+}
+
+
+
+function draw() {
+
+}
