c8d94a9b99211e071bb41601fae2f228604e0efd
alcho01 <alexbriancho@gmail.com>
2021-10-07 15:05:29 -0400

    Started age of aquariums exercise

---
+++ Exercises/age-of-aquariums/js/script.js
@@ -0,0 +1,31 @@
+/**
+Age of aquariums
+Alex Cho
+
+Brief
+User Controlled Shape[]
+User has to interact with "fish"[]
+Add more parameters(color,speed,size)[]
+Add a new property to the "fish" to change the behaviour[]
+Add two endings[]
+*/
+
+"use strict";
+
+
+
+function preload() {
+
+}
+
+
+
+function setup() {
+
+}
+
+
+
+function draw() {
+
+}
