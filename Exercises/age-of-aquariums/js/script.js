125c5712f9ca9e68c9caac764bdc7d7005714a1a
alcho01 <alexbriancho@gmail.com>
2021-10-13 14:06:46 -0400

    E4 added a function eatFish. When the user interacts with the fish they get eaten.

--- Exercises/age-of-aquariums/js/script.js
+++ Exercises/age-of-aquariums/js/script.js
@@ -12,6 +12,19 @@ Add two endings[]
 
 "use strict";
 
+let user = {
+  lives: 3,
+  x: undefined,
+  y: undefined,
+  w: 100,
+  h: 100,
+  vx: - 5,
+  vx2: 5,
+  vy: - 5,
+  vy2: 5,
+  image: undefined
+};
+
 //ARRAY FOR FISH
 let school = [];
 //AMOUNT OF FISH
@@ -35,19 +48,6 @@ let bg = {
   image: undefined,
 }
 
-//USER
-let user = {
-  x: undefined,
-  y: undefined,
-  w: 100,
-  h: 100,
-  vx: - 5,
-  vx2: 5,
-  vy: - 5,
-  vy2: 5,
-  image: undefined
-}
-
 function preload() {
   bg.image = loadImage('assets/images/background.png');
   user.image = loadImage('assets/images/user.png');
@@ -66,22 +66,22 @@ function setup() {
   user.y = height/2;
 
   for (let i = 0; i < schoolSize; i++) {
-    school[i] = createFish(random(0,width), random(0,height), random(0,5)); //CREATE FISH SETTINGS
+    school[i] = createFish(random(0,width), random(0,height),30, random(0,5)); //CREATE FISH SETTINGS
   }
   for (let i = 0; i < consortiumSize; i++) {
   consortium[i] = createCrab(random(0,width), random(0,height), random(0,7)); //CREATE CRAB SETTINGS
   }
 }
 
-function createFish(x,y,speed) {
+function createFish(x,y,w,speed) {
   let fish = {
     x: x,
     y: y,
-    w: 30,
+    w: w,
     h: 30,
     vx: 0,
     vy: 0,
-    speed: speed
+    speed: speed,
   };
   return fish;
 }
@@ -99,16 +99,14 @@ function createCrab(x,y,speed) {
   return crab;
 }
 
-
-
 function draw() {
   bgSet();
-
 //Fish
   for (let i = 0; i < school.length; i++) {
     moveFish(school[i]);
     displayFish(school[i]);
-  }
+    eatFish(school[i]);
+    }
 
 //Crabs
   for (let i = 0; i < consortium.length; i++) {
@@ -184,6 +182,19 @@ function displayFish(fish) {
   image(fishImg,fish.x,fish.y,fish.w,fish.h);
   pop();
 }
+
+function eatFish(fish) {
+  //IF FISH INTERACTS WITH USER IT GETS EATEN
+  for (let i = 0; i < school.length; i++) {
+    let fish = school[i];
+    let d = dist(user.x,user.y,fish.x,fish.y);
+    if (d < user.w / 2 + fish.w / 2) {
+      school.splice(i,1);
+      break;
+      }
+    }
+  }
+
 
 function moveCrab(crab) {
 //FISH MOVEMENT
