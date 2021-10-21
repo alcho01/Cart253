c97f0542a12d83fd11ea20e7cb7079cfc8645cfe
alcho01 <alexbriancho@gmail.com>
2021-10-21 14:52:07 -0400

    E5 Started Juggle Garden

---
+++ Exercises/JuggleGarden/js/script.js
@@ -0,0 +1,23 @@
+/**
+Juggle Garden
+Alex Cho
+
+Add a(nother) form of user-control[]
+Add a new class and objects[]
+Add at least two endings[]
+*/
+
+"use strict";
+
+function preload() {
+
+}
+
+
+function setup() {
+
+}
+
+function draw() {
+
+}
