dc76e987947422163440daddd211c67f883eb1e8
alcho01 <alexbriancho@gmail.com>
2021-10-21 16:25:03 -0400

    E5 Added the lily pad(paddle) created the canvas, added a background color(the water), added an image from illustrator

---
+++ Exercises/JuggleGarden/js/Lilypad.js
@@ -0,0 +1,23 @@
+class Lilypad {
+
+  constructor(w,h) {
+    this.width = w;
+    this.height = h;
+    this.x = 0;
+    this.y = height - this.height/2;
+  }
+
+//Move with mouse Added Constraints
+  move() {
+    this.x = constrain(mouseX,0,1200);
+    this.y = constrain(mouseY,570,700);
+  }
+
+  display(){
+    push();
+    fill(50,168,82); //green
+    noStroke();
+    ellipse(this.x,this.y,this.width,this.height);
+    pop();
+  }
+}
--- Exercises/JuggleGarden/js/script.js
+++ Exercises/JuggleGarden/js/script.js
@@ -9,15 +9,48 @@ Add at least two endings[]
 
 "use strict";
 
+let bgColor = {
+  r: 16,
+  g: 92,
+  b: 115,
+};
+
+let canvasSize = {
+  x: 1250,
+  y: 700,
+};
+
+let landscape = {
+  x: 625,
+  y: 350,
+  w: 1250,
+  h: 700,
+  image: undefined
+};
+
+let lilypad;
+
 function preload() {
+
+  landscape.image = loadImage('assets/images/landscape.png');
 
 }
 
-
 function setup() {
+  createCanvas(canvasSize.x,canvasSize.y);
 
+  lilypad = new Lilypad(250,30);
 }
 
 function draw() {
+  background(bgColor.r,bgColor.g,bgColor.b);
+
+//Landscape being Displayed
+  imageMode(CENTER);
+  image(landscape.image,landscape.x,landscape.y,landscape.w,landscape.h);
+
+//Lilypad being Displayed
+  lilypad.move();
+  lilypad.display();
 
 }
